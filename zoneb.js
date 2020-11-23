aae74d6777475cd78114b91da4ef96ebf25517ac
Chimenti <chimenti@seattleu.edu>
2020-11-23 11:52:27 -0800

    add zoneb scripts

---
+++ zoneb.js
@@ -0,0 +1,10 @@
+< script type = "text/javascript" >
+    (function() {
+        var x = document.createElement("script");
+        x.type = "text/javascript";
+        x.async = true;
+        x.src = (document.location.protocol === "https:" ? "https://" : "http://") + "libraryh3lp.com/js/libraryh3lp.js?14328";
+        var y = document.getElementsByTagName("script")[0];
+        y.parentNode.insertBefore(x, y);
+    })(); <
+/script>
